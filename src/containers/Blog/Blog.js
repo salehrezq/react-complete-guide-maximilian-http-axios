08a390bede900bbdc8f5c1fdadcd056714f866a7
Saleh Rezq <salehrezq@gmail.com>
2020-06-27 09:07:49 +0200

    Use data from axios and print them to the screen
    Lesson:
    09. Reaching out to the Web (Http  Ajax)\
    05. Rendering Fetched Data to the Screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,23 +7,28 @@ import './Blog.css';
 
 class Blog extends Component {
 
+   state = {
+      posts: []
+   }
+
    componentDidMount = () => {
 
       axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
          const posts = response.data;
-         posts.map((post) => {
-            console.log(post.title);
-         });
+         this.setState({posts: response.data});
       });
    }
 
    render() {
+
+      const posts = this.state.posts.map(post => {
+         return <Post key={post.id} title={post.title}/>;
+      });
+
       return (
          <div>
             <section className="Posts">
-               <Post/>
-               <Post/>
-               <Post/>
+               {posts}
             </section>
             <section>
                <FullPost/>
