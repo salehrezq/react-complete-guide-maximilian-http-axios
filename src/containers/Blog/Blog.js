ca5346ad57d6766aabf54cc22f097afcee4009e1
Saleh Rezq <salehrezq@gmail.com>
2020-06-27 11:10:39 +0200

    Make each post selectable
    Lesson:
    09. Reaching out to the Web (Http  Ajax)\
    07. Making a Post Selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 class Blog extends Component {
 
    state = {
-      posts: []
+      posts: [],
+      selectedPostId: null
    }
 
    componentDidMount = () => {
@@ -26,10 +27,19 @@ class Blog extends Component {
       });
    }
 
+   postSelectedHandler = (postId) => {
+      this.setState({selectedPostId: postId});
+   }
+
    render() {
 
       const posts = this.state.posts.map(post => {
-         return <Post key={post.id} title={post.title} author={post.author}/>;
+         return <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={(event) => this.postSelectedHandler(post.id)}
+         />;
       });
 
       return (
@@ -38,7 +48,7 @@ class Blog extends Component {
                {posts}
             </section>
             <section>
-               <FullPost/>
+               <FullPost postId={this.state.selectedPostId}/>
             </section>
             <section>
                <NewPost/>
