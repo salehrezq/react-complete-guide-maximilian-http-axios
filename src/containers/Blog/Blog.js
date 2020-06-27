e702d37d7d92c302fc903ee875852552671dea79
Saleh Rezq <salehrezq@gmail.com>
2020-06-27 07:41:04 +0200

    Initial usage of axios package
    Lesson:
    09. Reaching out to the Web (Http  Ajax)\
    04. Creating a Http Request to GET Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+   componentDidMount = () => {
+
+      axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+         const posts = response.data;
+         posts.map((post) => {
+            console.log(post.title);
+         });
+      });
+   }
+
    render() {
       return (
          <div>
