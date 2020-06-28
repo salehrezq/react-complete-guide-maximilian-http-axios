bb940de7f1dac8c3ab7f130df1be84db101ae220
Saleh Rezq <salehrezq@gmail.com>
2020-06-28 06:58:16 +0200

    Send axios DELETE request
    Lesson:
    09. Reaching out to the Web (Http  Ajax)\
    10. Sending a DELETE Request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -25,6 +25,12 @@ class FullPost extends Component {
       }
    }
 
+   deletePostHandler = () => {
+      axios.get('https://jsonplaceholder.typicode.com/posts/' + this.state.post.id).then((response) => {
+         console.log(response);
+      });
+   }
+
    render() {
       let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
@@ -38,7 +44,10 @@ class FullPost extends Component {
                <h1>{this.state.post.title}</h1>
                <p>{this.state.post.body}</p>
                <div className="Edit">
-                  <button className="Delete">Delete</button>
+                  <button
+                     onClick={this.deletePostHandler}
+                     className="Delete">Delete
+                  </button>
                </div>
             </div>
          );
