fd07dffd8a77c96f91a4baca7d224238ff5a0bd6
Saleh Rezq <salehrezq@gmail.com>
2020-07-03 08:44:59 +0200

    Add interceptors for the custom axios instance
    Lesson:
    09. Reaching out to the Web (Http  Ajax)\
    16. Creating and Using Axios Instances

--- src/axios.js
+++ src/axios.js
@@ -6,4 +6,34 @@ const instance = axios.create({
 
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 
+// The following interceptors are related to sending a request.
+instance.interceptors.request.use(request => {
+   console.log(request);
+   // You can edit the request config before sending it
+   // You must always return the request config,
+   // otherwise all sending requests will be blocked here
+   return request;
+}, error => {
+   console.log(error);
+   // Do something with the "request sending" error
+   // You must always do return this statement;
+   // so that request can reach to the `catch` block
+   return Promise.reject(error);
+});
+
+// The following interceptors are related to receiving a response.
+instance.interceptors.response.use(response => {
+   console.log(response);
+   // You can edit the response config before handling it in `then` block
+   // You must always return the response config,
+   // otherwise the response will not reach to the `then` block
+   return response;
+}, error => {
+   console.log(error);
+   // Do something with the "response" error
+   // You must always do return this statement;
+   // so that response error can reach to the `catch` block
+   return Promise.reject(error);
+});
+
 export default instance;
